97001d78d16c35383e1204b79b9e4ca3df83f98d
RAJENDRAN <abishanr30@gmail.com>
2024-11-13 13:51:49 +0100

    chat modifié

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Chat;
 use App\Repository\ChatRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,20 +11,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ChatController extends AbstractController
 {
     #[Route('/chat', name: 'chats')]
-    public function ListesChats(ChatRepository $repo): Response
+    public function ListesChats(ChatRepository $repo)
     {
         $repo->listeChatsComplete();/* query NOT result */
-        $request->query->getInt('page', 1); /*page number*/
         return $this->render('chat/index.html.twig', [
-            'controller_name' => 'ChatController',
+            'lesChats' => $chat,
         ]);
     }
 
     #[Route("/chat/{id}", name: 'ficheChat', methods :'GET')]
-    public function ficheChats(Artiste $artiste) : Response
+    public function ficheChats(Chat $chat)
     {
-        return $this->render('artiste/ficheChat.html.twig', [
-            'leArtiste' => $artiste
+        return $this->render('chat/ficheChat.html.twig', [
+            'leChat' => $chat
         ]);
     }
 }
