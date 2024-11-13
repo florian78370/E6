6f1c071e3f88890c830ad0d4f6b87fb6a9485560
Florian FORGET <florian.forget@btssio.dedyn.io>
2024-11-13 11:30:19 +0100

    pour branch florian

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ChatRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChatController extends AbstractController
 {
-    #[Route('/chat', name: 'app_chat')]
-    public function index(): Response
+    #[Route('/chat', name: 'chats')]
+    public function ListesChats(ChatRepository $repo): Response
     {
         return $this->render('chat/index.html.twig', [
             'controller_name' => 'ChatController',
