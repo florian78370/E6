fddf65e4d6a2d0c306addc761d559a2d4469ae04
Florian FORGET <florian.forget@btssio.dedyn.io>
2024-11-13 11:56:56 +0100

    COntroller

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -12,8 +12,18 @@ class ChatController extends AbstractController
     #[Route('/chat', name: 'chats')]
     public function ListesChats(ChatRepository $repo): Response
     {
+        $repo->listeChatsComplete();/* query NOT result */
+        $request->query->getInt('page', 1); /*page number*/
         return $this->render('chat/index.html.twig', [
             'controller_name' => 'ChatController',
+        ]);
+    }
+
+    #[Route("/chat/{id}", name: 'ficheChat', methods :'GET')]
+    public function ficheChats(Artiste $artiste) : Response
+    {
+        return $this->render('artiste/ficheChat.html.twig', [
+            'leArtiste' => $artiste
         ]);
     }
 }
