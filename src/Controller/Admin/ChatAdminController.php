9701a144835df40909a6f364a49f5be9633f4cd3
RAJENDRAN <abishanr30@gmail.com>
2024-11-21 11:43:01 +0100

    début ADMIN : Controller et form/list

--- src/Controller/Admin/ChatAdminController.php
+++ src/Controller/Admin/ChatAdminController.php
@@ -1,18 +1,72 @@
+
 <?php
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Album;
+use App\Form\AlbumType;
+use App\Repository\AlbumRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ChatController extends AbstractController
+class AlbumController extends AbstractController
 {
-    #[Route('/admin/chat', name: 'app_admin_chat')]
-    public function index(): Response
+    #[Route('/admin/albums', name: 'admin_albums', methods :'GET')]
+    public function listeAlbums(AlbumRepository $repo, PaginatorInterface $paginator, Request $request)
     {
-        return $this->render('admin/chat/index.html.twig', [
-            'controller_name' => 'ChatController',
+        $albums=$paginator->paginate(
+        $repo->listeAlbumsCompletePaginee(),
+        $request->query->getInt('page', 1), /*page number*/
+        8 /*limit per page*/
+        );
+        return $this->render('admin/album/listeAlbums.html.twig', [
+            'lesAlbums' => $albums
         ]);
     }
+
+    #[Route('/admin/albums/ajout', name: 'admin_album_ajout', methods :['GET', 'POST'])]
+    #[Route('/admin/albums/modif/{id}', name: 'admin_album_modif', methods :['GET', 'POST'])]
+    public function ajoutModifAlbum(Album $album=null, Request $request, EntityManagerInterface $manager)
+    {
+        if($album==null){
+            $album=new Album();
+            $mode="ajouté";
+        }else{
+            $mode="modifié";
+        }
+
+        $form=$this->createForm(AlbumType::class,$album);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid() )
+        {
+            $manager->persist($album);
+            $manager->flush();
+            $this->addFlash("success", "L'album a bien été $mode");
+            return $this->redirectToRoute('admin_albums');
+        }
+        return $this->render('admin/album/formAjoutModifAlbum.html.twig', [
+            'formAlbum' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/admin/albums/supression/{id}', name: 'admin_album_supression', methods :'GET')]
+    public function supressionAlbum(Album $album, EntityManagerInterface $manager)
+    {
+        $nbMorceaux=$album->getMorceaux()->count();
+        if($nbMorceaux>0){
+            $this->addFlash("danger", "Malheureusement, vous ne pouvez pas supprimer cette album car $nbMorceaux morceau(x) y sont associés");
+        }else{
+            $manager->remove($album);
+            $manager->flush();
+            $this->addFlash("success", "L'album a bien été supprimé");
+        }
+
+        return $this->redirectToRoute('admin_albums');
+    }
+    
 }
